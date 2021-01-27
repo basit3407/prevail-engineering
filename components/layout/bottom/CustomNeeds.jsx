01994b0e65767d027b4ffbde910679c9ea1e852a
Basit Minhas <basit3407@gmail.com>
2021-01-27 09:45:25 +0500

    customNeeds/about section completed

--- components/layout/bottom/CustomNeeds.jsx
+++ components/layout/bottom/CustomNeeds.jsx
@@ -1,3 +1,55 @@
+import {
+  Button,
+  Card,
+  Container,
+  Grid,
+  Typography,
+  Zoom,
+} from "@material-ui/core";
+import useStyles from "../../../styles/materialUI/useStyles";
+
 export default function CustomNeeds() {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <Card className={classes.customNeedsCard}>
+      <Container>
+        <Grid container>
+          <Zoom timeout={400} in={true}>
+            <Grid
+              item
+              className={classes.globalGridItem}
+              style={{ padding: "5%" }}
+            >
+              <Typography className={classes.customNeedsHeading} variant="h1">
+                CUSTOM NEEDS ?
+              </Typography>
+              <Typography
+                style={{
+                  padding: "3%",
+                }}
+                variant="body1"
+              >
+                More than often, our clients will need personalized solutions,
+                and this is our speciality, with our expertise we are certain
+                your needs and requirements will be met.
+              </Typography>
+              <Button
+                style={{
+                  backgroundColor: "#fff",
+                  borderRadius: 0,
+                  padding: "1.5% 3%",
+                }}
+                size="large"
+                href="/contact"
+              >
+                <Typography style={{ color: "#0062ae" }} variant="caption">
+                  LETS GET STARTED
+                </Typography>
+              </Button>
+            </Grid>
+          </Zoom>
+        </Grid>
+      </Container>
+    </Card>
+  );
 }
