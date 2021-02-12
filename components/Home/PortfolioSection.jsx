6c9f3d28cff7492a5d1ed598cf363c1d74802b9b
Basit Minhas <basit3407@gmail.com>
2021-02-12 09:41:20 +0500

    work in progress

--- components/Home/PortfolioSection.jsx
+++ components/Home/PortfolioSection.jsx
@@ -1,11 +1,12 @@
-import { Card, Container, Grid, Typography } from "@material-ui/core";
+import { Card, Container, Grid, Typography, Fade } from "@material-ui/core";
 import useStyles from "../../styles/materialUI/MuiuseStyles";
-import FadeIn from "../FadeIn";
 import { ArrayMap } from "../Gallery/Images";
+import { useInView } from "react-intersection-observer";
 
 export default function PortfolioSection() {
   //Assign CSS
   const classes = useStyles(),
+    [ref, inView] = useInView({ triggerOnce: true }),
     array = [
       { number: 1, typography: "Dolmen HVAC Works" },
       { number: 3, typography: "Atrium Mall AHU Controlling" },
@@ -17,7 +18,7 @@ export default function PortfolioSection() {
     <Card className={classes.portfolioCard}>
       <Container>
         <Grid container className={classes.portfolioGrid} spacing={3}>
-          <FadeIn timeout={1000}>
+          <Fade in={inView} ref={ref} timeout={2000}>
             <Grid className={classes.globalGridItem} item xs={12}>
               <Typography className={classes.portfolioHeading} variant="h1">
                 GALLERY
@@ -26,7 +27,7 @@ export default function PortfolioSection() {
                 Our work samples and portfolios
               </Typography>
             </Grid>
-          </FadeIn>
+          </Fade>
 
           <ArrayMap array={array} />
         </Grid>
